0c73331da98be9c5cb2ce53c3fe0b54b5acea993
Khashayar <pouradaby@gmail.com>
2022-07-31 11:25:01 +0430

    Listing page changes

--- partials_card.php
+++ partials_card.php
@@ -31,7 +31,21 @@
             <h3 class="uk-margin-small-bottom">
                 <a href="" class="uk-text-secondary uk-h6 uk-margin-remove font f700 uk-text-truncate uk-display-inline-block" title="">عنوان کسب و کار اینجا قرار میگیرد</a>
             </h3>
-            <p class="uk-text-secondary uk-text-tiny font f500 uk-text-truncate uk-margin-remove">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه</p>
+            <p class="uk-text-secondary uk-text-tiny font f500 uk-text-truncate uk-margin-remove-top uk-margin-small-bottom">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه</p>
+            <ul class="uk-flex uk-padding-remove uk-margin-remove menu">
+                <li class="uk-margin-left">
+                    <a href="#">
+                        <i class="far fa-fw fa-map-marker-alt"></i>
+                        <span class="value">تهران</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="tel:546654644646">
+                        <i class="far fa-fw fa-phone"></i>
+                        <span class="value ss02">54644646</span>
+                    </a>
+                </li>
+            </ul>
         </div>
         <div class="card-footer uk-padding-small">
             <div class="uk-grid-small" data-uk-grid>
@@ -42,12 +56,12 @@
                     </a>
                 </div>
                 <div class="uk-width-auto">
-                    <a href="" title="نشان کردن" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip>
+                    <a href="" title="نشان کردن" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip="pos: top-right">
                         <span class="uk-flex uk-flex-middle uk-flex-center uk-border-circle icon"><i class="far fa-heart"></i></span>
                     </a>
                 </div>
                 <div class="uk-width-auto">
-                    <a href="" title="اطلاعات بیشتر" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip>
+                    <a href="" title="اطلاعات بیشتر" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip="pos: top-left">
                         <span class="uk-flex uk-flex-middle uk-flex-center uk-border-circle icon"><i class="far fa-search"></i></span>
                     </a>
                 </div>
