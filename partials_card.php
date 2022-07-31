ef2cc8d573e9ea7dd7d327e1b111e77afcf2db8d
Khashayar <pouradaby@gmail.com>
2022-07-31 11:17:16 +0430

    Listing page

---
+++ partials_card.php
@@ -0,0 +1,57 @@
+<div>
+    <div class="cardWrapper">
+        <div class="uk-position-relative card-image">
+            <div class="uk-position-top-right uk-padding-small uk-position-z-index labels uk-text-zero">
+                <span class="uk-label uk-label-success uk-box-shadow-small">جدید</span>
+                <span class="uk-label uk-label-danger uk-box-shadow-small">ویژه</span>
+            </div>
+            <div class="uk-position-bottom-right uk-padding-small uk-position-z-index uk-text-zero uk-flex">
+                <div class="uk-flex uk-flex-middle ratings">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <span class="font f700 uk-text-tiny">(۱۵۹ نظر)</span>
+            </div>
+            <div class="uk-position-absolute uk-position-z-index circleLogo">
+                <img src="img/logo.webp" width="40" height="40" alt="" class="uk-border-circle">
+            </div>
+            <a href="#" title="" class="uk-display-block">
+                <figure class="uk-padding-tiny uk-margin-remove">
+                    <div class="uk-cover-container uk-border-rounded uk-overflow-hidden">
+                        <canvas width="400" height="250"></canvas>
+                        <img src="https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="" height="" alt="" class="" data-uk-cover>
+                    </div>
+                </figure>
+            </a>
+        </div>
+        <div class="card-content uk-padding-small">
+            <h3 class="uk-margin-small-bottom">
+                <a href="" class="uk-text-secondary uk-h6 uk-margin-remove font f700 uk-text-truncate uk-display-inline-block" title="">عنوان کسب و کار اینجا قرار میگیرد</a>
+            </h3>
+            <p class="uk-text-secondary uk-text-tiny font f500 uk-text-truncate uk-margin-remove">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه</p>
+        </div>
+        <div class="card-footer uk-padding-small">
+            <div class="uk-grid-small" data-uk-grid>
+                <div class="uk-width-expand">
+                    <a href="" title="نشان کردن" class="uk-flex uk-flex-middle">
+                        <span class="uk-flex uk-flex-middle uk-flex-center uk-border-circle icon category pink"><i class="far fa-scissors"></i></span>
+                        <span class="uk-margin-small-right title">آرایشگاه‌های زنانه</span>
+                    </a>
+                </div>
+                <div class="uk-width-auto">
+                    <a href="" title="نشان کردن" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip>
+                        <span class="uk-flex uk-flex-middle uk-flex-center uk-border-circle icon"><i class="far fa-heart"></i></span>
+                    </a>
+                </div>
+                <div class="uk-width-auto">
+                    <a href="" title="اطلاعات بیشتر" class="uk-flex uk-flex-center uk-flex-middle" data-uk-tooltip>
+                        <span class="uk-flex uk-flex-middle uk-flex-center uk-border-circle icon"><i class="far fa-search"></i></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- ui-elements.php
+++ ui-elements.php
@@ -2,8 +2,197 @@
 <?php include ('partials_head.php'); ?>
 <body>
 <?php include ('partials_header.php'); ?>
-<?php include ('partials_page_head.php'); ?>
-<main>aaaaa</main>
+<main class="uk-padding-large uk-padding-remove-horizontal">
+    <div class="uk-container">
+        <div class="uk-child-width-1-1 uk-child-width-1-2@s uk-grid-large" data-uk-grid>
+            <div>
+                <h3 class="font f700 uk-h4">آکاردیون</h3>
+                <div>
+                    <ul data-uk-accordion>
+                        <li class="uk-open">
+                            <a class="uk-accordion-title" href="#">عنوان پرسش شماره یک</a>
+                            <div class="uk-accordion-content">
+                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a class="uk-accordion-title" href="#">عنوان پرسش شماره دو</a>
+                            <div class="uk-accordion-content">
+                                <p>دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a class="uk-accordion-title" href="#">عنوان پرسش شماره سه</a>
+                            <div class="uk-accordion-content">
+                                <p>دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد</p>
+                                <p>دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">استایل ۱ تب</h3>
+                <div>
+                    <ul data-uk-tab>
+                        <li class="uk-active"><a href="#">آیتم فعال</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li class="uk-disabled"><a>تب غیر فعال</a></li>
+                    </ul>
+                    <ul class="uk-tab-primary" data-uk-tab>
+                        <li class="uk-active"><a href="#">آیتم فعال</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                    </ul>
+                    <ul class="uk-tab-accent" data-uk-tab>
+                        <li class="uk-active"><a href="#">آیتم فعال</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                    </ul>
+                </div>
+                <h3 class="font f700 uk-h4">استایل ۲ تب</h3>
+                <div>
+                    <ul class="uk-subnav uk-subnav-pill" data-uk-switcher="animation: uk-animation-fade">
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                    </ul>
+                    <ul class="uk-subnav uk-subnav-pill uk-subnav-primary" data-uk-switcher="animation: uk-animation-fade">
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                    </ul>
+                    <ul class="uk-subnav uk-subnav-pill uk-subnav-accent" data-uk-switcher="animation: uk-animation-fade">
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                        <li><a href="#">عنوان تب</a></li>
+                    </ul>
+                    <div class="uk-switcher uk-margin-top">
+                        <div>محتوای تب شماره یک</div>
+                        <div>محتوای تب شماره دو</div>
+                        <div>محتوای تب شماره سه</div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">پیام های هشدار</h3>
+                <div>
+                    <div class="uk-alert-primary" data-uk-alert>
+                        <a class="uk-alert-close" data-uk-close></a>
+                        <p>این پیام برای اعلان هشدار های عمومی است.</p>
+                    </div>
+
+                    <div class="uk-alert-success" data-uk-alert>
+                        <a class="uk-alert-close" data-uk-close></a>
+                        <p>این پیام برای اعلان هشدار با بار مثبت است.</p>
+                    </div>
+
+                    <div class="uk-alert-warning" data-uk-alert>
+                        <a class="uk-alert-close" data-uk-close></a>
+                        <p>این پیام برای اعلان هشدار برای جلب توجه است.</p>
+                    </div>
+
+                    <div class="uk-alert-danger" data-uk-alert>
+                        <a class="uk-alert-close" data-uk-close></a>
+                        <p>این پیام برای اعلان هشدار با بار منفی است.</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">نوتیفیکیشن ها</h3>
+                <div class="uk-flex uk-flex-wrap">
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'success', pos: 'top-left'})"><span>بالا چپ مثبت</span></button>
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'warning', pos: 'top-center'})"><span>بالا وسط هشدار</span></button>
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'danger', pos: 'top-right'})"><span>بالا راست منفی</span></button>
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'success', pos: 'bottom-left'})"><span>پایین چپ مثبت</span></button>
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'warning', pos: 'bottom-center'})"><span>پایین وسط هشدار</span></button>
+                    <button class="uk-button uk-button-primary uk-margin-left uk-margin-bottom" type="button" onclick="UIkit.notification({message: 'متن نوتیفیکیشن اینجا قرار می گیرد', status: 'danger', pos: 'bottom-right'})"><span>پایین راست منفی</span></button>
+                </div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">دکمه ها</h3>
+                <div>ffffff</div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">المان های فرم</h3>
+                <div>
+                    <form>
+                        <fieldset class="uk-fieldset">
+
+                            <div class="uk-margin">
+                                <label>
+                                    <input class="uk-input" type="text" placeholder="ورودی متنی  عادی">
+                                </label>
+                            </div>
+                            <div class="uk-margin">
+                                <label>
+                                    <input class="uk-input uk-form-danger" type="text" placeholder="ورودی متنی  عادی">
+                                </label>
+                            </div>
+                            <div class="uk-margin">
+                                <label>
+                                    <input class="uk-input uk-form-success" type="text" placeholder="ورودی متنی  عادی">
+                                </label>
+                            </div>
+
+                            <div class="uk-margin">
+                                <label>
+                                    <select class="uk-select">
+                                        <option>گزینه شماره یک</option>
+                                        <option>گزینه شماره دو</option>
+                                    </select>
+                                </label>
+                            </div>
+
+                            <div class="uk-margin">
+                                <label>
+                                    <textarea class="uk-textarea" rows="5" placeholder="ناحیه متنی"></textarea>
+                                </label>
+                            </div>
+
+                            <div class="uk-child-width-1-2" data-uk-grid>
+                                <div>
+                                    <div class="uk-margin">
+                                        <input class="uk-radio" type="radio" name="radio2" id="radio1" checked>
+                                        <label for="radio1"> عنوان گزینه اول</label>
+                                    </div>
+                                    <div class="uk-margin">
+                                        <input class="uk-radio" type="radio" name="radio2" id="radio2">
+                                        <label for="radio2"> عنوان گزینه دوم</label>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-margin">
+                                        <input class="uk-checkbox" type="checkbox" id="checkbox1" checked>
+                                        <label for="checkbox1"> عنوان گزینه اول</label>
+                                    </div>
+                                    <div class="uk-margin">
+                                        <input class="uk-checkbox" type="checkbox" id="checkbox2">
+                                        <label for="checkbox2"> عنوان گزینه دوم</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">تولتیپ</h3>
+                <div>ffffff</div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">لیست دارای آیکن</h3>
+                <div>ffffff</div>
+            </div>
+            <div>
+                <h3 class="font f700 uk-h4">جدول ساده</h3>
+                <div>ffffff</div>
+            </div>
+        </div>
+    </div>
+</main>
 <?php include ('partials_footer.php'); ?>
 </body>
 </html>
